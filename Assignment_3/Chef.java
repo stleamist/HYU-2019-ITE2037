9fe67c412914bb9682cb7007c4f3fa7fdb7bfeca
Dongkyu Kim <stleamist@me.com>
2019-06-17 20:38:33 +0900

    Complete Assignment 3

--- Assignment_3/Chef.java
+++ Assignment_3/Chef.java
@@ -14,40 +14,41 @@ class Chef extends Thread {
 	
 	public void run() {
 		for (Dish dish: dishes) {
-			int time = dish.getTime();
-			
-			switch (dish.getTool()) {
-				case "oven":
-					kitchen.useOven();
-					break;
-				case "fryingpan":
-					kitchen.useFriedPan();
-					break;
-				case "pot":
-					kitchen.usePot();
-					break;
+			try {
+				int time = dish.getTime();
+				
+				switch (dish.getTool()) {
+					case "oven":
+						kitchen.useOven();
+						break;
+					case "fryingpan":
+						kitchen.useFriedPan();
+						break;
+					case "pot":
+						kitchen.usePot();
+						break;
+				}
+				
+				Thread.sleep(time);
+				
+				String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(System.currentTimeMillis());
+				
+				System.out.printf("Table #%d / %s is completed by %s. %s\n", dish.getTableNum(), dish.getName(), this.name, date);
+				
+				switch (dish.getTool()) {
+					case "oven":
+						kitchen.releaseOven();
+						break;
+					case "fryingpan":
+						kitchen.releaseFriedPan();
+						break;
+					case "pot":
+						kitchen.releasePot();
+						break;
+				}
+			} catch (InterruptedException e) {
+				System.out.println(e);
 			}
-			
-			Thread.sleep(time);
-			
-			String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(System.currentTimeMillis());
-			
-			System.out.printf("Table #%d / %s is completed by %s. %s\n", dish.getTableNum(), dish.getName(), this.name, date);
-			
-			switch (dish.getTool()) {
-				case "oven":
-					kitchen.releaseOven();
-					break;
-				case "fryingpan":
-					kitchen.releaseFriedPan();
-					break;
-				case "pot":
-					kitchen.releasePot();
-					break;
-			}
-			
 		}
-		
-		
 	}
 }
--- Assignment_3/Restaurant.java
+++ Assignment_3/Restaurant.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 class Restaurant {
 	static TableSet tbset;
@@ -20,7 +21,42 @@ class Restaurant {
 		kang.start();
 	}
 	
-	static void devideDishes() {
+	static TableSet loadingData() {
+		TableSet tableSet = new TableSet();
+		int index = -1;
+		int tableNum = -1;
 		
+		try {
+			File file = new File("OrderSheet.txt");
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			String line = "";
+			while((line = bufferedReader.readLine()) != null){
+				
+				if (line.startsWith("Table")) {
+					String tableNumString = line.substring(line.length() - 1);
+					tableNum = Integer.parseInt(tableNumString);
+					index += 1;
+				} else {
+					tableSet.addToTable(index, tableNum, line);
+				}
+			}
+			
+			bufferedReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		} catch (IOException e){
+			System.out.println(e);
+		}
+		
+		return tableSet;
+	}
+	
+	static void devideDishes() {
+		for (int i = 0; i < 5; i++) {
+			ArrayList<Dish> dishes = (i % 2 == 0) ? dishes_lee : dishes_kang;
+			dishes.addAll(tbset.getTableSet()[i].getDishSet());
+		}
 	}
 }
